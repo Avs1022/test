a5b2d83122c3c0e99fe6b09fd2e55b1298ab2461
Avs1022 <110104202+Avs1022@users.noreply.github.com>
2023-11-13 17:34:42 +0530

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
+  constructor(){
+  }
+  
   title = 'myapp';
+  private navbutton=[
+    {
+      text:"home",
+      href:'/homepage'
+    },
+    {
+      text:"loginscreen",
+      href:'/loginscreen'
+    },
+    {
+      text:'productpage',
+      href:'/productpage'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes,RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginscreenComponent } from './loginscreen/loginscreen.component';
+import { ProductpageComponent } from './productpage/productpage.component';
 
+const routes :Routes =[
+  {path:'homepage',component:HomepageComponent},
+  {path:'loginscreen',component:LoginscreenComponent},
+  {path:'productpage',component:ProductpageComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginscreenComponent,
+    ProductpageComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[RouterModule]
 })
 export class AppModule { }
